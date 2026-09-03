a35e17539b1cf1323e009c5aa822f3d709379ad8
zcorp <zcorp@zcorps-Mac-mini.lan>
2026-09-03 14:10:25 +0200

    Publish static site only

--- app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  ...(process.env.DEPLOY_MODE === "github-pages" ? { output: "export" } : {}),
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-};
-
-module.exports = nextConfig;
